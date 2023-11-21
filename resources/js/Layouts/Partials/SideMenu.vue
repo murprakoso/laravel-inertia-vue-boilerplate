a3561d5e482aed788175f7e648aaf99237a1bae6
Eko Prakoso <prakoso.clan04@gmail.com>
2023-11-22 00:42:33 +0700

    fix(SideMenu): activation menu by url or path

--- resources/js/Layouts/Partials/SideMenu.vue
+++ resources/js/Layouts/Partials/SideMenu.vue
@@ -4,54 +4,63 @@ import {router} from "@inertiajs/vue3";
 import {items} from "@/Utils/menuItem.js";
 
 const state = reactive({
-  collapsed: false,
-  selectedKeys: ['dashboard'],
-  openKeys: [],
-  preOpenKeys: [],
+    collapsed: false,
+    selectedKeys: [],
+    openKeys: [],
+    preOpenKeys: [],
 });
 
 
 const handleClick = (e) => {
-  // Get key from parent key if exists
-  const parentKey = e.key.length > 1 ? e.keyPath[0] : e.key;
+    const parentKey = e.key.length > 1 ? e.keyPath[0] : e.key;
+    const activeMenuData = {parent: parentKey, key: e.key};
+    localStorage.setItem("activeMenu", JSON.stringify(activeMenuData));
 
-  // Save data as an object in Local Storage
-  const activeMenuData = {parent: parentKey, key: e.key};
-  localStorage.setItem('activeMenu', JSON.stringify(activeMenuData));
+    state.selectedKeys = [e.key];
+    state.openKeys = [parentKey];
+    state.preOpenKeys = [parentKey];
 
-  state.selectedKeys = [e.key];
-  state.openKeys = [parentKey];
-  state.preOpenKeys = [parentKey];
-
-  // Navigate using router
-  router.visit('/' + e.key);
+    router.visit("/" + e.key);
 };
 
-watch(() => state.openKeys, (val) => {
-  console.log('openKeys', val);
-});
+const setActiveKeys = () => {
+    const {pathname} = window.location;
+    const splitPathname = pathname.split("/");
+    const currentPath = splitPathname[splitPathname.length - 1];
+
+    const menuItem = items.find((item) => {
+        if (item.key === currentPath) {
+            return true;
+        } else if (item.children) {
+            return item.children.some((child) => child.key === currentPath);
+        }
+        return false;
+    });
+
+    if (menuItem) {
+        if (menuItem.children) {
+            state.selectedKeys = [currentPath];
+            state.openKeys = [menuItem.key];
+        } else {
+            state.selectedKeys = [menuItem.key];
+            state.openKeys = [];
+        }
+    }
+};
 
 onMounted(() => {
-  const savedActiveMenu = localStorage.getItem('activeMenu');
-  if (savedActiveMenu !== null) {
-    const activeMenuData = JSON.parse(savedActiveMenu);
-    const {parent, key} = activeMenuData;
-
-    state.selectedKeys = [key];
-    state.openKeys = [parent];
-    state.preOpenKeys = [parent];
-  }
-})
+    setActiveKeys();
+});
 </script>
 
 <template>
-  <a-menu
-      v-model:openKeys="state.openKeys"
-      v-model:selectedKeys="state.selectedKeys"
-      mode="inline"
-      theme="dark"
-      :inline-collapsed="state.collapsed"
-      :items="items"
-      @click="handleClick"
-  ></a-menu>
+    <a-menu
+        v-model:openKeys="state.openKeys"
+        v-model:selectedKeys="state.selectedKeys"
+        mode="inline"
+        theme="dark"
+        :inline-collapsed="state.collapsed"
+        :items="items"
+        @click="handleClick"
+    ></a-menu>
 </template>
