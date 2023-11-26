0d9e2ae739ba06a5f90f9745406d43348ddb41c0
Eko Prakoso <prakoso.clan04@gmail.com>
2023-11-27 00:13:12 +0700

    refactor(DefaultLayout): adding dropdown and link(logout, profile)

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -1,45 +1,66 @@
 <script setup>
 import Sidebar from "@/Layouts/Partials/Sidebar.vue";
+import {DownOutlined} from '@ant-design/icons-vue'
+import {router} from '@inertiajs/vue3';
 import {VueQueryDevTools} from "vue-query/devtools";
+
+
+const gotoProfile = async () => {
+  return router.get('/profile')
+}
+
+const handleLogout = async () => {
+  return router.post('/logout')
+}
 </script>
 
 <template>
-    <div>
-        <VueQueryDevTools :initialIsOpen="false" position="bottom-right"/>
-    </div>
+  <div>
+    <VueQueryDevTools :initialIsOpen="false" position="bottom-right"/>
+  </div>
 
-    <a-layout style="min-height: 100vh">
-        <Sidebar/>
-        <a-layout>
-            <a-layout-header>
-                <div class="flex justify-between px-4">
-                    <div class="font-medium">
-                        <slot name="header"/>
-                    </div>
+  <a-layout style="min-height: 100vh">
+    <Sidebar/>
+    <a-layout>
+      <a-layout-header>
+        <div class="flex justify-between px-4">
+          <div class="font-medium">
+            <slot name="header"/>
+          </div>
 
-                    <div>
-                        <span class="font-medium">
-                            {{ $page.props.auth.user.name }}
-                        </span>
+          <div>
+            <a-dropdown :trigger="['click']">
+              <a class="ant-dropdown-link font-medium text-black" @click.prevent>
+                {{ $page.props.auth.user.name }}
+                <DownOutlined/>
+              </a>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item key="0" @click="gotoProfile">
+                    <a href="">Profile</a>
+                  </a-menu-item>
+                  <a-menu-divider/>
+                  <a-menu-item key="1" @click="handleLogout">
+                    Logout
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
+          </div>
+        </div>
+      </a-layout-header>
+      <a-layout-content style="margin: 20px 16px">
+        <!--                <a-breadcrumb style="margin: 16px 0">-->
+        <!--                    <a-breadcrumb-item>User</a-breadcrumb-item>-->
+        <!--                    <a-breadcrumb-item>Bill</a-breadcrumb-item>-->
+        <!--                </a-breadcrumb>-->
 
-                        <!--                        <DropdownLink :href="route('logout')" method="post" as="button">-->
-                        <!--                            Log Out-->
-                        <!--                        </DropdownLink>-->
-                    </div>
-                </div>
-            </a-layout-header>
-            <a-layout-content style="margin: 20px 16px">
-                <!--                <a-breadcrumb style="margin: 16px 0">-->
-                <!--                    <a-breadcrumb-item>User</a-breadcrumb-item>-->
-                <!--                    <a-breadcrumb-item>Bill</a-breadcrumb-item>-->
-                <!--                </a-breadcrumb>-->
+        <slot/>
 
-                <slot/>
-
-            </a-layout-content>
-            <a-layout-footer style="text-align: center">
-                ©2018 Developed by Murprakoso
-            </a-layout-footer>
-        </a-layout>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        ©2018 Developed by Murprakoso
+      </a-layout-footer>
     </a-layout>
+  </a-layout>
 </template>
