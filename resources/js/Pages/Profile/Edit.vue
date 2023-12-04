bedbde6aa12c2faea95c4e25e9c287e80147a3cc
Eko Prakoso <prakoso.clan04@gmail.com>
2023-12-05 00:57:43 +0700

    refactor(Profile): adding update profile information

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,48 +1,23 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import DeleteUserForm from './Partials/DeleteUserForm.vue';
-import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
-import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
-import {Head, usePage} from '@inertiajs/vue3';
-import DefaultLayout from "@/Layouts/DefaultLayoutV2.vue";
-import DashboardLayout from "@/Layouts/DefaultLayout.vue";
-
-defineProps({
-  mustVerifyEmail: {
-    type: Boolean,
-  },
-  status: {
-    type: String,
-  },
-});
+import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+import {Head} from '@inertiajs/vue3';
+import UpdateProfileInformationForm from "@/Pages/Profile/Partials/UpdateProfileInformationForm.vue";
 </script>
 
 <template>
   <Head title="Profile"/>
 
-  <DashboardLayout>
+  <DefaultLayout>
     <template #header>
       Profile
     </template>
 
-    <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-          <UpdateProfileInformationForm
-              :must-verify-email="mustVerifyEmail"
-              :status="status"
-              class="max-w-xl"
-          />
-        </div>
-
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-          <UpdatePasswordForm class="max-w-xl"/>
-        </div>
-
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-          <DeleteUserForm class="max-w-xl"/>
-        </div>
-      </div>
-    </div>
-  </DashboardLayout>
+    <a-card>
+      <a-row :gutter="16" class="mb-6">
+        <a-col :span="12">
+          <UpdateProfileInformationForm/>
+        </a-col>
+      </a-row>
+    </a-card>
+  </DefaultLayout>
 </template>
---
+++ resources/js/Pages/ProfileV2/Edit.vue
@@ -0,0 +1,48 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import DeleteUserForm from './Partials/DeleteUserForm.vue';
+import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
+import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
+import {Head, usePage} from '@inertiajs/vue3';
+import DefaultLayout from "@/Layouts/DefaultLayoutV2.vue";
+import DashboardLayout from "@/Layouts/DefaultLayout.vue";
+
+defineProps({
+  mustVerifyEmail: {
+    type: Boolean,
+  },
+  status: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <Head title="Profile"/>
+
+  <DashboardLayout>
+    <template #header>
+      Profile
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+          <UpdateProfileInformationForm
+              :must-verify-email="mustVerifyEmail"
+              :status="status"
+              class="max-w-xl"
+          />
+        </div>
+
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+          <UpdatePasswordForm class="max-w-xl"/>
+        </div>
+
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+          <DeleteUserForm class="max-w-xl"/>
+        </div>
+      </div>
+    </div>
+  </DashboardLayout>
+</template>
